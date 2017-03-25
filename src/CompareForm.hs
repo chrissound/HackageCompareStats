d0f7e24ae255333fa52200beebfe207ea287d8ea
Chris Stryczynski <chris@soundshare.co.uk>
2017-03-25 19:08:54 +0000

    Redirect package to have alphabetical segments. Prevents acces to duplicate content on alternative URL

--- src/CompareForm.hs
+++ src/CompareForm.hs
@@ -11,22 +11,35 @@ import           Data.Maybe                           (isJust)
 import           Data.String.Conversions
 import           Prelude
 import           UserError                            (getErrorTmpl)
-import Web.Scotty.Trans (rescue, ActionT, raise, params, Param)
+import Web.Scotty.Trans (rescue, ActionT, raise, Param, redirect)
 import qualified Data.Text.Internal.Lazy as LText (Text)
 import Control.Monad.Trans.Reader
+import Data.List (sort, intercalate)
 
 processParams :: Maybe [Param] -> Maybe [Param]
 processParams = fmap . fmap $ f
   where f (p, _) = (("package[]"), p)
 
 getStore :: ArchCompareActionM APSs
-getStore  = lift $ Common.getStore <$> ask
+getStore  = do
+  lift $ Common.getStore <$> ask
+
+getURL :: [String] -> String
+getURL = ((++) "/comparePackage/") . intercalate "/"
+
+getExposeURL :: String -> ArchCompareActionM String
+getExposeURL x = do
+  archConfig <- lift ask
+  return $ (++) (getBaseUrl archConfig) x
 
 comparePackageHandler :: ArchCompareActionM ()
 comparePackageHandler = do
   archConfig <- lift ask
   store <- getStore
   requestedPackages' <- requestedPackages
+  when (requestedPackages' /= sort requestedPackages')
+    $ redirect $ convertString
+    $ getURL $ convertString <$> sort requestedPackages'
   rescue (do
     when ( not $ length requestedPackages' >= 2) $
       raise "You need to specify atleast two requestedPackages"
